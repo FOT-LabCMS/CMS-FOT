73ac7104d14fa3dda36a46f7aacd99de5a2eacd0
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2026-09-04 10:53:34 +0530

    Update allowed origins in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -48,7 +48,8 @@ app.set("trust proxy", 1);
 const allowedOrigins = [
   "http://localhost:5173", // Your local frontend
   "https://fotcms.onrender.com", // Your deployed frontend
-  "http://192.248.50.135:5173", // Your deployed frontend
+  "http://192.248.50.135", // Your deployed frontend
+  "http://teclcms.duckdns.org"
 ];
 
 // Dynamically add the FRONTEND_URL from the .env file to the whitelist if it's not already there.
